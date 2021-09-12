ec4a738bcf61fbd8405507539929a3cc8b812472
satishbabu <reachsatish@yahoo.com>
2021-09-11 22:49:19 -0400

    added query with params

--- src/main/java/com/apnatriangle/graphqldemo/Query.java
+++ src/main/java/com/apnatriangle/graphqldemo/Query.java
@@ -8,4 +8,8 @@ public class Query implements GraphQLQueryResolver {
     public String firstQuery() {
         return "First Query";
     }
+
+    public String fullName(String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
 }
